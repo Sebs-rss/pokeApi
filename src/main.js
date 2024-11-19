5c62ea231186cab043ddc0c052e56b673a1d90cb
Sebs-rss <seba.r.sanchez@gmail.com>
2024-11-19 18:57:20 -0300

    Patch

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+// Bootstrap para mejorar la apariencia de avisos de errores
+/* import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; */
+
 createApp(App).mount('#app')
